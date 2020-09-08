b42e5c1d022f8379a911d094100cebd12bcd7306
DanielJang <daniel.jang.0621@gmail.com>
2020-09-08 15:15:14 +0900

    hash-q2

---
+++ Hash/PermutationPalindrome.py
@@ -0,0 +1,53 @@
+import unittest
+
+
+def has_palindrome_permutation(the_string):
+
+    # Check if any permutation of the input is a palindrome
+
+    list = []
+
+    for letter in the_string:
+        if (letter not in list):
+            list.append(letter)
+        else:
+            list.remove(letter)
+
+    if len(list) > 1:
+        return False 
+
+    return True
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_permutation_with_odd_number_of_chars(self):
+        result = has_palindrome_permutation('aabcbcd')
+        self.assertTrue(result)
+
+    def test_permutation_with_even_number_of_chars(self):
+        result = has_palindrome_permutation('aabccbdd')
+        self.assertTrue(result)
+
+    def test_no_permutation_with_odd_number_of_chars(self):
+        result = has_palindrome_permutation('aabcd')
+        self.assertFalse(result)
+
+    def test_no_permutation_with_even_number_of_chars(self):
+        result = has_palindrome_permutation('aabbcd')
+        self.assertFalse(result)
+
+    def test_empty_string(self):
+        result = has_palindrome_permutation('')
+        self.assertTrue(result)
+
+    def test_one_character_string(self):
+        result = has_palindrome_permutation('a')
+        self.assertTrue(result)
+
+
+unittest.main(verbosity=2)
