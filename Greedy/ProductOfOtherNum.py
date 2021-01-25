10a4b335e65638c6d77bf21bc490ac6db5171780
DanielJang <daniel.jang.0621@gmail.com>
2021-01-25 23:48:58 +0900

    Jan 25th commit

---
+++ Greedy/ProductOfOtherNum.py
@@ -0,0 +1,76 @@
+import unittest
+
+
+def get_products_of_all_ints_except_at_index(int_list):
+
+    # Make a list with the products
+    
+    list_length = len(int_list)
+    
+    if list_length < 2:
+        raise ValueError('Less than 2 items in list!')
+    
+    
+    list_to_return = [1] * list_length 
+    
+    for i in range(len(int_list)):
+        counter = 0 
+        while(counter!=list_length):
+            if (counter < i):
+                list_to_return[counter] = list_to_return[counter] * int_list[i]
+                counter +=1 
+            elif (counter == i):
+                counter+=1 
+            elif (counter < list_length):
+                list_to_return[counter] = list_to_return[counter] * int_list[i]
+                counter+=1 
+
+    return list_to_return 
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_small_list(self):
+        actual = get_products_of_all_ints_except_at_index([1, 2, 3])
+        expected = [6, 3, 2]
+        self.assertEqual(actual, expected)
+
+    def test_longer_list(self):
+        actual = get_products_of_all_ints_except_at_index([8, 2, 4, 3, 1, 5])
+        expected = [120, 480, 240, 320, 960, 192]
+        self.assertEqual(actual, expected)
+
+    def test_list_has_one_zero(self):
+        actual = get_products_of_all_ints_except_at_index([6, 2, 0, 3])
+        expected = [0, 0, 36, 0]
+        self.assertEqual(actual, expected)
+
+    def test_list_has_two_zeros(self):
+        actual = get_products_of_all_ints_except_at_index([4, 0, 9, 1, 0])
+        expected = [0, 0, 0, 0, 0]
+        self.assertEqual(actual, expected)
+
+    def test_one_negative_number(self):
+        actual = get_products_of_all_ints_except_at_index([-3, 8, 4])
+        expected = [32, -12, -24]
+        self.assertEqual(actual, expected)
+
+    def test_all_negative_numbers(self):
+        actual = get_products_of_all_ints_except_at_index([-7, -1, -4, -2])
+        expected = [-8, -56, -14, -28]
+        self.assertEqual(actual, expected)
+
+    def test_error_with_empty_list(self):
+        with self.assertRaises(Exception):
+            get_products_of_all_ints_except_at_index([])
+
+    def test_error_with_one_number(self):
+        with self.assertRaises(Exception):
+            get_products_of_all_ints_except_at_index([1])
+
+
+unittest.main(verbosity=2)
--- Greedy/readme.md
+++ Greedy/readme.md
@@ -28,3 +28,21 @@ No "shorting"—you need to buy before you can sell. Also, you can't buy and sel
 Given a list of integers, find the highest product you can get from three of the integers.
 
 The input list_of_ints should always have at least three integers.
+
+# Highest Product of Other Numbers
+
+You have a list of integers, and for each index you want to find the product of every integer except the integer at that index.
+
+Write a function get_products_of_all_ints_except_at_index() that takes a list of integers and returns a list of the products.
+
+For example, given:
+
+```
+[1, 7, 3, 4]
+# your function will return
+[84, 12, 28, 21]
+# by calculating
+[7 * 3 * 4,  1 * 3 * 4,  1 * 7 * 4,  1 * 7 * 3]
+```
+
+Here's the catch: **You can't use division in your solution!**
